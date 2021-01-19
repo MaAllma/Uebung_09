e16e19c66b09595ec01e8b4e26133f15afece0f7
Magdalena Allmann <allmann@rhrk.uni-kl.de>
2021-01-19 12:30:21 +0100

    added comments

--- main.cpp
+++ main.cpp
@@ -8,37 +8,30 @@
 
 
 /**
-* determines wether a file is .fa
-**/
-// bool isFa(std::string fileName) {
-	
-// 	//TODO: make check more than just last letter
-	
-// 	int size = fileName.size() - 1;
-	
-// 	if (fileName[size] == 'a') {
-// 		return true;
-// 	} else {
-// 		return false;
-// 	}
-	
-	
-// }
-
-/**
  *  Münzwurffolge
  */
 std::vector<int> sequence; 
-// a_begin,fair
+
+/**
+ *  a_begin,fair
+ */
 float transition_begin_fair;
-// a_i,j
+
+/**
+ *  a_i,j
+ */
 float transition_changeCoin;
-// p_gezinkt(0)
+
+/**
+ *  p_gezinkt(0)
+ */
 float p_manipulated_number;
 
 
 /**
  * returns the probability to get a 0 or a 1 with the given coin
+ * @param state is the coin
+ * @param coin_result is eather 0 or 1 for number or head
  */
 float probability (int state, int coin_result){
 	switch (state)
@@ -80,50 +73,67 @@ int main(int argc, char* argv[])
 		// a_begin,fair = eine Zahl zwischen 0 und 1
 		// a_i,j = Zahl zwischen 0 und 1
 		// p_gezinkt(0) = Zahl zwischen 0 und 1
+
 	if(argc == 5) {
 		
 		//reading
 
-		//Wurffolge
+		
+
 		//das einlesen der Zahlenfolge funktioniert noch nicht so ganz :/
 		//meine Idee war es das CharArray durchzugehen und jedes char einzeln als Zahl zu konvertieren, aber das scheint so nicht ganz zu gehen
 		//auch die Erkennung, dass es sich um eine 0 handelt klappt noch nicht so ganz :/
+
+		/* <--- 1. argument: Wurffolge ---> */
 		char* numbers = argv[1];
-		std::cerr << "numbers: " << numbers << std::endl;
 		size_t length = 3;
-		std::cerr  << "lenght: " <<  length << std::endl << std::endl;
+
+		//Zur Kontrolle:
+		std::cout  << "lenght: " <<  length << std::endl;
+		std::cout << "numbers: " << numbers << std::endl;
+
+		//char stückweise durchgehen, zu int konvertieren und in den sequence vector einfügen
 		for (int i = 0; i < length; i++) {
+
+			//Konvertieren des char zu int
 			char c = numbers[i];
 			int number = (int)c;
 			number = number - 48;
 			
+			//überprüfen ob die Zahlenfolge nur aus 0en oder 1en besteht
 			if (number != 0 || number != 1){
-				std::cerr << "char: " << c << std::endl;
-				std::cerr << "int: " << number << std::endl;
-				std::cerr << "The sequence can only contain 0 or 1\n\n";
+
+				//zum Verständniss::
+				std::cout << "char: " << c << std::endl;
+				std::cout << "int: " << number << std::endl;
+
+				//wirkliche Fehlermeldung:
+				std::cerr << "The sequence can only contain 0 or 1\n";
 				//return -1;
 			}
 			
+			//Sequenz in der die Münzwurfabfolge gespeichert wird
 			sequence.push_back(number);
-			std::cerr << "Sequence size" << sequence.size() << std::endl;
+			std::cout << "Sequence size" << sequence.size() << std::endl;
 		}
 
 
-		// a_begin,fair
+		/* <--- 2. argument: a_begin,fair ---> */
 		transition_begin_fair = (float)strtod((argv[2]),NULL);
 		if (transition_begin_fair < 0 || transition_begin_fair > 1){
 			std::cerr << "The probability must be between 0 and 1.\n";
 			return -1;
 		}
 
-		// a_i,j
+		/* <--- 3. argument: a_i,j ---> */
 		transition_changeCoin = (float)strtod((argv[3]),NULL);
 		if (transition_changeCoin < 0 || transition_changeCoin > 1){
 			std::cerr << "The probability must be between 0 and 1.\n";
 			return -1;
 		}
 
-		// p_gezinkt(0)
+
+		/* <--- 4. argument: p_gezinkt(0)---> */
 		p_manipulated_number = (float)strtod((argv[4]),NULL);
 		if (p_manipulated_number < 0 || p_manipulated_number > 1){
 			std::cerr << "The probability must be between 0 and 1.\n";
@@ -131,9 +141,11 @@ int main(int argc, char* argv[])
 		}
 
 		//initialize table
+			//rows = verschiedene Zustände: gezinkt, fair, begin
+			//cols = Länge der Sequenz + eine Spalte zur Initialisierung
 		Matrix table(3, sequence.size()+1);
 
-		//row 0
+		/* <--- row 0 ---> */
 
 		//fair
 		table(0,0) = 0;
@@ -141,36 +153,37 @@ int main(int argc, char* argv[])
 		table(1,0) = 0;
 		//begin
 		table(2,0) = 1;
-		//std::cerr << "nullte Reihe erstellt\n";
 
-		//row 1
+		/* <--- row 1 ---> */
 		for (size_t z = 0; z < 2; z++){
 			table(z,1) = log10(probability(z,sequence[0])) + 1 + log10(transition(2,z));
 		}
-		//std::cerr << "erste Reihe erstellt\n";
 
-		//other rows
+		/* <--- other rows ---> */
 		for (size_t i = 2; i < sequence.size()+1; i ++){
 			for (size_t z = 0; z < 2; z++){
 				table(z,i) = log10(probability(z,sequence[i-1])) + std::max(table(0,i-1) + log10(transition(0,z)), table(1,i-1) + log10(transition(1,z)));
 			}
-			//std::cerr << i << "te Reihe erstellt\n";
 		}
 
-		//backtracking
+		/* <--- backtracking ---> */
+
+		//Vector in dem die Abfolge der Münzen gespeichert wird = Ausgabe des Programms
 		std::vector<char> result;
 
-		//letzte Münze
+		//letzte Münze (statt end)
 		char last_coin;
+		//max der beiden Zeilen
 		if (table(0, sequence.size()) > table(1, sequence.size())) last_coin = 'f';
 		else last_coin = 'g';
+		//füge die letzte Münze vorne in das Ergebnis an
 		result.insert(result.begin(), last_coin);
-		//std::cerr << "last coin = " << last_coin << std::endl;
 
 		
 		//Backtracking:
-
 		for (size_t i = sequence.size(); i > 1; i--){
+			//rückwärts durch die Matrix gehen
+			//wähle das Feld der aktuell letzten Münze und suche von dort aus das Maximum
 			if (last_coin == 'f'){
 				if (table(0,i-1) + log10(transition(0,0)) > table(1,i-1) + log10(transition(1,0))){
 					last_coin = 'f';
@@ -182,6 +195,7 @@ int main(int argc, char* argv[])
 				}
 				else last_coin = 'g';
 			}
+			//füge die Münze vorne in das Ergebnis an
 			result.insert(result.begin(), last_coin);
 			
 		}
@@ -201,7 +215,11 @@ int main(int argc, char* argv[])
 	//program is used with to little or to many arguments
 	} else {
 		//std:: cerr << argc;
-		std::cerr << "Run program with:\n- a sequence of coin toss,\n- a probability for starting with the fair coin,\n- a probability for changing the coin,\n- a probability to get the number with the manipulated coin" << std::endl;
+		std::cerr << "Run program with:\n" <<
+		"- a sequence of coin toss,\n" <<
+		"- a probability for starting with the fair coin,\n" <<
+		"- a probability for changing the coin,\n" <<
+		"- a probability to get the number with the manipulated coin" << std::endl;
 			return -3;
 		
 	}
